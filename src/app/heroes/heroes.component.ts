c78af9196fdf65bef269bd903fe47b9d4f0790ba
Ruth Johnston <ruth.johnston@symphonytalent.com>
2021-07-08 10:56:20 +0100

    adds click event handler to heroComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  
   heroes = HEROES;
-
+  selectedHero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
